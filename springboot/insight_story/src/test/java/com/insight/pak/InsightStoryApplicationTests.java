dd4fc589d6fea882f9a0bb3dbcc5b6d16c714863
kimbongju02 <qhdwn25@gmail.com>
2024-05-08 11:31:51 +0900

    Initial setting

---
+++ springboot/insight_story/src/test/java/com/insight/pak/InsightStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insight.pak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
