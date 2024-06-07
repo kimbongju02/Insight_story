cfa8c9ef0e9da22acea393eeb8809e1eda616331
sujin-727 <166008218+sujin-727@users.noreply.github.com>
2024-06-07 23:11:45 +0900

    Add files via upload

---
+++ insight_story/src/main/java/com/insight/pak/InsightStoryApplication.java
@@ -0,0 +1,13 @@
+package com.insight.pak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsightStoryApplication.class, args);
+	}
+
+}
